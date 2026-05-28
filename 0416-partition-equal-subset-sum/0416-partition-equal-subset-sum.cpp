2587635ffcd54e07395d88ef7a39296780fa6439
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-05-28 16:58:44 +0530

    Time: 152 ms (32.15%), Space: 17.5 MB (57.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -25,9 +25,25 @@ public:
         }
         if(totalSum % 2 != 0){
             return false;
-        }
+        } 
         int target = totalSum / 2;
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return f(nums, n-1, target, dp);
+        vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        if(nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+        for(int i = 1; i < n; i++){
+            for(int  j = 1; j <= target; j++){
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if(nums[i] <= j){
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = take || notTake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
