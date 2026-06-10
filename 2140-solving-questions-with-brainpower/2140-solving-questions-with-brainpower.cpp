6096be894c74bcd3ade55b882d9c0f435640085a
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-06-11 01:12:31 +0530

    Time: 12 ms (27.32%), Space: 127.7 MB (13%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long f(int idx, vector<vector<int>>& q, vector<long long>& dp){
+        if(idx >= q.size()) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        long long take = q[idx][0] + f(idx+q[idx][1]+1, q, dp);
+        long long notTake = f(idx+1, q, dp);
+        return dp[idx] = max(take, notTake);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        int m = questions[0].size();
+        vector<long long>dp(n+1, -1);
+        return f(0, questions, dp);
+    }
+};
