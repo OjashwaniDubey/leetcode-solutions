68e2645b63969fbe40e6244816604ace2e65fbd9
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-06-21 23:56:04 +0530

    Time: 40 ms (25.5%), Space: 80.2 MB (61.09%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int ans = 0;
+        sort(costs.begin(), costs.end());
+        for(int cost : costs){
+            if(coins < cost) break;
+            coins -= cost;
+            ans++;
+        }
+        return ans;
+    }
+};
