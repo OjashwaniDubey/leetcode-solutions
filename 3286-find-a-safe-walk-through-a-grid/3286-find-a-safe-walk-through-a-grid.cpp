f622f936661702288f7ca6d9f7569bfe272c77c2
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-07-02 22:43:58 +0530

    Time: 14 ms (74.11%), Space: 33 MB (40.83%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int n = grid.size();
+        int m = grid[0].size();
+        deque<pair<int, int>> dq;
+        vector<vector<int>>dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = grid[0][0];
+        dq.push_front({0,0});
+        vector<pair<int, int>> dir = {
+            {1,0},{0,1},{-1,0},{0,-1}
+        };
+        while(!dq.empty()){
+            auto [x, y] = dq.front();
+            dq.pop_front();
+            for(auto[dx, dy] : dir){
+                int nx = x + dx;
+                int ny = y + dy;
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m){
+                    continue;
+                }
+                int weight = grid[nx][ny];
+                int newdist = dist[x][y] + weight;
+                if(newdist < dist[nx][ny]){
+                    dist[nx][ny] = newdist;
+                    if(weight == 0){
+                    dq.push_front({nx, ny});
+                    }
+                    else{
+                    dq.push_back({nx, ny});
+                    }
+                }
+            }
+        }
+        if(dist[n-1][m-1] == INT_MAX) return false;
+        else if(health - dist[n-1][m-1] >= 1) return true;
+        return false;
+    }
+};
