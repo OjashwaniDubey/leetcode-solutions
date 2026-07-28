078d4c82b929701d6c29aec7fb6331075d4ea3f7
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-07-28 14:15:04 +0530

    Time: 0 ms (100%), Space: 9.1 MB (86.49%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int> freq(52, 0);
-        string left = "";
-        string middle = "";
+        //vector<int> freq(52, 0);
+        //string left = "";
+        //string middle = "";
+        //for(char c : s){
+        //    if(islower(c)){
+        //        freq[c-'a']++;
+        //    }
+        //    else freq[c-'A'+26]++;
+        //}
+        //for(int i = 0; i < 52; i++){
+        //    char ch;
+        //    if(i < 26){
+        //        ch = 'a'+i;
+        //    }
+        //    else ch = 'A'+i-26;
+        //    left.append(freq[i]/2, ch);
+        //    if(freq[i]%2 && middle.empty()){
+        //        middle = ch;
+        //    }
+        //}
+        //string right = left;
+        //reverse(right.begin(), right.end());
+        //string ans = left + middle + right;
+        //return ans.size();
+        vector<int> freq(128, 0);
+        int ans = 0;
         for(char c : s){
-            if(islower(c)){
-                freq[c-'a']++;
-            }
-            else freq[c-'A'+26]++;
+            freq[c]++;
         }
-        for(int i = 0; i < 52; i++){
-            char ch;
-            if(i < 26){
-                ch = 'a'+i;
-            }
-            else ch = 'A'+i-26;
-            left.append(freq[i]/2, ch);
-            if(freq[i]%2 && middle.empty()){
-                middle = ch;
-            }
+        bool odd = false;
+        for(int x : freq){
+            ans += (x/2)*2;
+            if(x % 2) odd = true;
         }
-        string right = left;
-        reverse(right.begin(), right.end());
-        string ans = left + middle + right;
-        return ans.size();
+        if(odd) ans++;
+        return ans;
     }
 };
