6491d1a5e65162d6662637c00502696a25296576
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-08-01 14:28:21 +0530

    Time: 2 ms (43.58%), Space: 10.1 MB (52.91%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -9,7 +9,17 @@ public:
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, n-1, nums, dp) >= 0;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; i++){
+            dp[i][i] = nums[i];
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i+1; j < n; j++){
+                int takeLeft = nums[i] - dp[i+1][j];
+                int takeRight = nums[j] - dp[i][j-1];
+                dp[i][j] = max(takeLeft, takeRight);
+            }
+        }
+        return dp[0][n-1] >= 0;
     }
 };
