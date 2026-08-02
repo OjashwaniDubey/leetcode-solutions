8b7c75db127b4b393a95e50b1fe4f44ecd788344
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-08-02 19:52:51 +0530

    Time: 69 ms (92.53%), Space: 88.6 MB (60.22%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -13,11 +13,21 @@ public:
     }
     int stoneGameVII(vector<int>& stones) {
         int n = stones.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
         vector<int> prefix(n+1, 0);
         for(int i = 0; i < n; i++){
             prefix[i+1] = prefix[i] + stones[i];
         }
-        return f(0, n-1, stones, prefix, dp);
+        for(int i = 0; i < n; i++){
+            dp[i][i] = 0;
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i+1; j < n; j++){
+                int takeLeft = prefix[j+1] - prefix[i+1] - dp[i+1][j];
+                int takeRight = prefix[j] - prefix[i] - dp[i][j-1];
+                dp[i][j] = max(takeLeft, takeRight);
+            }
+        }
+        return dp[0][n-1];
     }
 };
