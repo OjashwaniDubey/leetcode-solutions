bc6612a3abcd61dab64023a3e01277b9a232e9a6
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-08-06 11:09:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -11,6 +11,15 @@ public:
         return f(n+1, t);
     }
     int smallestNumber(int n, int t) {
-        return f(n, t);
+        while(true){
+        int product = 1;
+        int temp = n;
+        while(temp){
+            product *= temp % 10;
+            temp /= 10;
+        }
+        if(product % t == 0) return n;
+        n++;
+        }
     }
 };
