82624bd17886d972df41f9275da36291552158a9
OjashwaniDubey <ojaswanidubey1@gmail.com>
2026-09-04 15:56:56 +0530

    Time: 0 ms (100%), Space: 98.1 MB (80.5%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefixMax(n);
-        vector<int> suffixMin(n);
-        prefixMax[0] = nums[0];
+        //vector<int> prefixMax(n);
+        //vector<int> suffixMin(n);
+        //prefixMax[0] = nums[0];
+        //for(int i = 1; i < n; i++){
+        //    prefixMax[i] = max(prefixMax[i-1], nums[i]);
+        //}
+        //suffixMin[n-1] = nums[n-1];
+        //for(int i = n-2; i >= 0; i--){
+        //    suffixMin[i] = min(suffixMin[i+1], nums[i]);
+        //}
+        //for(int i = 0; i < n; i++){
+        //    if(prefixMax[i] <= suffixMin[i+1]) return i+1;
+        //}
+        //return -1;
+        int leftMax = nums[0];
+        int maxSoFar = nums[0];
+        int partition = 1;
         for(int i = 1; i < n; i++){
-            prefixMax[i] = max(prefixMax[i-1], nums[i]);
+            maxSoFar = max(maxSoFar, nums[i]);
+            if(nums[i] < leftMax){
+                leftMax = maxSoFar;
+                partition = i + 1;
+            }
         }
-        suffixMin[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--){
-            suffixMin[i] = min(suffixMin[i+1], nums[i]);
-        }
-        for(int i = 0; i < n; i++){
-            if(prefixMax[i] <= suffixMin[i+1]) return i+1;
-        }
-        return -1;
+        return partition;
     }
 };
